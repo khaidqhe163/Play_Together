3e82b0c0c49da33eaa5548eff6e32eb3f65f010b
khaidqhe163 <167944897+khaidqhe163@users.noreply.github.com>
2024-05-28 10:43:08 +0700

    update login

--- front-end/src/components/Login.jsx
+++ front-end/src/components/Login.jsx
@@ -2,23 +2,29 @@ import React, { useRef, useState } from 'react'
 import { Col, Container, Row, Form } from 'react-bootstrap'
 import "../css/login.css"
 import LoginLeft from './LoginLeft'
-import { useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import axios from 'axios'
 import { useDispatch } from 'react-redux'
 import { setUserInformation } from '../features/userSlice'
+import { setAccessToken } from '../features/accessTokenSlice'
+import { setRefreshToken } from '../features/refreshTokenSlice'
+
 import ForgotPassword from './ForgotPassword'
 function Login() {
     const nav = useNavigate();
     const email = useRef();
     const password = useRef();
     const dispatch = useDispatch();
-
     const [showResetPassword, setShowResetPassword] = useState(false);
 
     const handleClose = () => setShowResetPassword(false);
     const handleShow = () => setShowResetPassword(true);
     const handleLogin = async (e) => {
         e.preventDefault();
+        if (email.current.value === "" || password.current.value === "") {
+            alert("Vui lòng nhập đẩy đủ thông tin!");
+            return;
+        }
         try {
             const userInfo = await axios.post(`http://localhost:3008/api/user/login`, {
                 email: email.current.value,
@@ -31,7 +37,9 @@ function Login() {
             }
             )
             console.log(userInfo);
-            dispatch(setUserInformation(userInfo.data));
+            dispatch(setUserInformation(userInfo.data.user));
+            dispatch(setAccessToken(userInfo.data.accessToken))
+            dispatch(setRefreshToken(userInfo.data.refreshToken))
             nav('/')
         } catch (error) {
             console.log(error);
@@ -55,9 +63,11 @@ function Login() {
                 <Col md={6} id='login-rightside'>
                     <div id='login-header'>
                         <button onClick={() => { nav('/register') }}>Đăng ký</button>
-                        <div id='login-close'>
-                            <ion-icon name="close-outline"></ion-icon>
-                        </div>
+                        <Link to="/">
+                            <div id='login-close'>
+                                <ion-icon name="close-outline"></ion-icon>
+                            </div>
+                        </Link>
                     </div>
                     <div id='formlogin'>
                         <h1>Đăng nhập</h1>
