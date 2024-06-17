2207e71ad3902c0e0bef8c3a9d82cb988090e8b0
tungnhhe161576 <tungnhhe161576@fpt.edu.vn>
2024-06-17 13:11:58 +0700

    crud comment

--- back-end/services/CommentService.js
+++ back-end/services/CommentService.js
@@ -4,7 +4,7 @@ import Comment from '../models/Comment.js';
 
 export const getAllCommentsByStoryId = async (storyID) => {
     try {
-        const comments = await Comment.find({ storyId: storyID }).populate('commentor', ['username', 'avatar']);
+        const comments = await Comment.find({ storyId: storyID }).populate('commentor', ['username', 'avatar']).populate('reply', ['username']);
         return comments;
     } catch (error) {
         throw new Error("Error fetch comments: " + error.message);
@@ -20,8 +20,31 @@ const createComment = async (data) => {
     }
 }
 
+const deleteComment = async (commentId) => {
+    try {
+        const result = await Comment.findByIdAndDelete(commentId)
+        if (!result) {
+            throw new Error('Comment not found')
+        }
+        return result
+    } catch (error) {
+        throw new Error(error.message);
+    }
+}
+
+const updateComment = async (updateData, commentId) => {
+    try {
+        const updatedComment = await Comment.findByIdAndUpdate(commentId, updateData, { new: true })
+        return updatedComment
+    } catch (error) {
+        throw new Error('Error updating comment');
+    }
+};
+
 
 export default {
     getAllCommentsByStoryId,
     createComment,
+    deleteComment,
+    updateComment,
 }
